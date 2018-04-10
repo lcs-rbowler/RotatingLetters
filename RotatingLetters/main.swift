aa150ef1f409c0203ff6992f1adc157e1307bc0b
Ryan Bowler <rbowler@lcs.on.ca>
2018-04-10 10:06:07 -0400

    1 Solution completed

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -11,7 +11,38 @@ import Foundation
 // Get the user input
 var rawInput = readLine()
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+//Make sure input is not nil
+guard let input = rawInput else
+{
+    //error
+    exit(9)
+}
+
+
+var useableCharacters : Bool = true
+
+
+// Input
+
+characterCheck: for individualCharacter in input
+{
+
+    // Catagorize the character
+    switch individualCharacter {
+    case "I", "O", "H", "Z", "N", "S":
+        useableCharacters = true
+    default:
+        useableCharacters = false
+        break characterCheck // Do nothing
+    }
+}
+
+// Output
+
+if useableCharacters == true {
+    print("Yes")
+} else if useableCharacters == false {
+    print("No")
+}
+
 
